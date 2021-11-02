34e9310c51a4ae328dbc3a89191e311aa02cc2c8
jshubham895 <jshubham895@gmail.com>
2021-11-02 18:09:52 +0530

    1809 hrs

---
+++ shubham/shubham/SumClosest.java
@@ -0,0 +1,20 @@
+package shubham;
+
+import java.util.Arrays;
+
+public class SumClosest {
+    public static void main(String[] args) {
+        int[] nums = {-1,2,1,-4};
+        int target = 1;
+        System.out.println(threeSumClosest(nums, target));
+    }
+
+    public static int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int ans = 0;
+
+        
+
+        return ans;
+    }
+}
